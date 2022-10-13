2032c452770b95587ae544e75f885d6b883899bb
Van Asbrouck Gaspard <210546@CCM-Asty-Moulin.local>
2022-10-13 14:11:35 +0200

    ajout du formulaire2

---
+++ Formulaire2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Un super formulaire</h1>
+    <fieldset>
+        <legend>Données personnelles</legend>
+        <div>
+            <label for="prenom">Votre prénom</label>
+            <input type="text" name="prenom" id="prenom">
+        </div>
+        <div>
+            <label for="nom">Votre nom</label>
+            <input type="text" name="nom" id="nom">
+        </div>
+        <div>
+            <label for="datedenaissance">Votre date de naissance</label>
+            <input type="date" name="datedenaissance" id="datedenaissance">
+        </div>
+
+    </fieldset>
+</body>
+</html>
